692a822d4ffd6f4890124c05f86e22fc6b06eab8
Matt McCormick <matt@mbmccormick.com>
2015-01-03 11:31:34 -0800

    Adding diagnostic page to allow emptying queue for any stuck messages.

---
+++ WebRole/Controllers/QueueController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Microsoft.ServiceBus.Messaging;
+using WorkerRole.Models;
+
+namespace WebRole.Controllers
+{
+    public class QueueController : Controller
+    {
+        public ActionResult Delete()
+        {
+            ViewBag.MessageCount = QueueConnector.GetQueue().MessageCount;
+
+            BrokeredMessage message = QueueConnector.QueueClient.Receive(new TimeSpan(0, 0, 3));
+
+            while (message != null)
+            {
+                message.Complete();
+
+                message = QueueConnector.QueueClient.Receive(new TimeSpan(0, 0, 3));
+            }
+
+            ViewBag.DeletedMessageCount = ViewBag.MessageCount - QueueConnector.GetQueue().MessageCount;
+
+            return View();
+        }
+    }
+}
